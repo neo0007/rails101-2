61652c81b3074d7d98fb090b0d05e0eb79937331
Neo <mr.yliu@gmail.com>
2017-04-05 12:37:50 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,10 +1,15 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @posts = current_user.posts
   end
 
-  def delete
+  def destroy
+    
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to account_posts_path
   end
-  
+
 end
